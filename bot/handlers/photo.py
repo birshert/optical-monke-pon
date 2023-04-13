f4432c4b97a07752c54409364cfdde43c881c65a
birshert <birshertalexey21@gmail.com>
2023-04-14 00:29:11 +0300

    Model deployment + bot ready baseline

--- bot/handlers/photo.py
+++ bot/handlers/photo.py
@@ -1,7 +1,8 @@
 import asyncio
+import base64
 import os
 
-from PIL import Image
+import aiohttp
 from aiogram import Router, F, Bot
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
@@ -13,17 +14,11 @@ logger = get_bot_logger()
 router = Router()
 
 
-def save_image(image, path):
-    images_count = len(os.listdir(path))
-    image_path = os.path.join(path, f"{images_count + 1:>04}.png")
-
-    image.save(image_path)
-    logger.info(f"Downloaded image: {image_path}")
-
-
 @router.message(F.photo)
 async def incoming_photo(message: Message, bot: Bot, state: FSMContext):
     user_id = message.from_user.id
+
+    logger.info(f"Got photo from {user_id}")
 
     user_images_path = os.path.join(images_path, f"{user_id}")
     os.makedirs(user_images_path, exist_ok=True)
@@ -32,10 +27,16 @@ async def incoming_photo(message: Message, bot: Bot, state: FSMContext):
     image_file = await bot.get_file(image.file_id)
     file = await bot.download_file(image_file.file_path)
 
-    img = Image.open(file)
+    base64_encoded_data = base64.b64encode(file.read())
+    base64_message = base64_encoded_data.decode('utf-8')
 
-    save_image(img, user_images_path)
+    async with aiohttp.request(
+            method="post",
+            url="http://0.0.0.0:5000/predictions",
+            json={"input": {"image": f"data:image/png;base64,{base64_message}"}}
+    ) as model_prediction:
+        model_prediction = await model_prediction.json()
+        logger.info(f"Got prediction for {user_id}: {model_prediction}")
+        await message.reply(model_prediction["output"])
 
     await asyncio.sleep(0.5)
-
-    await message.reply("Фотография загружена")
---
+++ deployment/predict.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+from PIL import Image
+from cog import BasePredictor, Path, Input
+from torchvision.models import resnet18
+
+
+def to_real_price(price):
+    return ((torch.exp(price) - 1) * 1000).item()
+
+
+class Predictor(BasePredictor):
+    def setup(self):
+        self.model = resnet18()
+        self.model.fc = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.LeakyReLU(),
+            nn.Linear(512, 1)
+        )
+
+        self.model.load_state_dict(torch.load("weights.pth", map_location="cpu"))
+
+        self.transforms = T.Compose(
+            [
+                T.Resize((224, 224)),
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        self.model.to(self.device)
+        self.model.eval()
+
+    @torch.inference_mode()
+    def predict(self, image: Path = Input(description="Image")) -> float:
+        image = self.transforms(Image.open(image).convert("RGB")).unsqueeze(0).to(self.device)
+
+        return to_real_price(self.model(image))
