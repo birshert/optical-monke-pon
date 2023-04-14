e1f4a3cc70da15bdc3c444c1e6f74e80337c33a1
derpinyou <55846013+derpinyou@users.noreply.github.com>
2023-04-14 21:27:18 +0400

    Update base_commands.py

--- bot/handlers/base_commands.py
+++ bot/handlers/base_commands.py
@@ -2,6 +2,7 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+from random import randint
 
 from utils.logs import get_bot_logger
 
@@ -14,9 +15,14 @@ router = Router()
 async def info(message: Message, state: FSMContext):
     logger.info(f"Started for user {message.from_user.id}")
     await message.answer("Добро пожаловать!")
+    
 
-
-@router.message(Command(commands=["meme"]))
+@router.message(Command(commands=["I_wanna_random_monkey_sticker"]))
 async def info(message: Message, state: FSMContext):
     logger.info(f"Meme user {message.from_user.id}")
-    await message.answer_sticker(sticker="CAACAgIAAxkBAAEIlK1kOYMjfpbNqMaupdq0SfakOBeqMwAChxcAAgzwWErywdtxMQ5ycC8E")
+    
+    with open('monkey_stickers_ids.txt') as f:
+        monkey_stickers_ids = f.read().split('\n')
+    sticker_id = monkey_stickers_ids[randint(0, 9)]
+    
+    await message.answer_sticker(sticker=sticker_id)
